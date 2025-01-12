6a7098521196f3af81d2f6d6711b6064d9ba8c84
Aarya2311 <150429517+Aaryafalle@users.noreply.github.com>
2025-01-12 15:42:04 +0530

    Add files via upload

---
+++ script60.js
@@ -0,0 +1,46 @@
+console.log("hello there")
+let a = "schinchan"
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+console.log(a[7]);
+console.log(a[8]);    
+
+let first_name = "Aarya"
+let last_name = "vedanti"
+
+console.log(`the girl name ${first_name} has a bestie named ${last_name}`);
+console.log(first_name.length);
+console.log(last_name.length);
+
+console.log(first_name.toUpperCase());
+console.log(last_name.toLowerCase());
+console.log(first_name.charAt(4));
+console.log(first_name.indexOf('a'));
+console.log(first_name.slice(1,4));
+console.log(first_name.concat(last_name,a));
+
+
+// //hello there
+// s
+// c
+// h
+// i
+// n
+// c
+// h
+// a
+// n
+// the girl name Aarya has a bestie named vedanti
+// 5
+// 7
+// AARYA
+// vedanti
+// a
+// 1
+// ary
+// Aaryavedantischinchan
